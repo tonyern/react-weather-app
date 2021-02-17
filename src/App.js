fcfe661aecc32d2e1e287d14b5676311717c74be
Tony <tony.d.nguyen@protonmail.com>
2021-02-17 13:15:08 -0600

    Added search-bar.css for better organization.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import "./index.css";
+import "./search-bar.css";
 import WeatherInfo from "./components/weather-display/WeatherInfo";
 
 const App = () => {
