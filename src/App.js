4be6096615474595028a2021b64b45e84aedbed8
Tony <tony.d.nguyen@protonmail.com>
2021-02-24 10:16:22 -0600

    Seperated search bar.

--- src/App.js
+++ src/App.js
@@ -1,50 +1,10 @@
-import React, { useEffect, useRef, useState } from "react";
-import axios from "axios";
+import React, { useState } from "react";
 import "./index.css";
-import "./search-bar.css";
+import SearchBar from "./components/search-bar/SearchBar";
 import WeatherInfo from "./components/weather-display/WeatherInfo";
 
 const App = () => {
-  const openWeatherMapAPI = {
-    key: "8713314e929a6ae57041a78e979b402d",
-    base: "http://api.openweathermap.org/data/2.5/",
-  };
-
-  const [query, setQuery] = useState("");
   const [weather, setWeather] = useState({});
-  const inputRef = useRef();
-
-  useEffect(() => {
-    inputRef.current.focus();
-  });
-
-  const search = (evt) => {
-    if (evt.key === "Enter") {
-      axios
-        .get(
-          `${openWeatherMapAPI.base}weather?q=${query}&units=metric&APPID=${openWeatherMapAPI.key}`
-        )
-        .then((response) => {
-          setQuery("");
-          setWeather(response.data);
-          console.log(response.data);
-        })
-        .catch((error) => {
-          setQuery("");
-
-          if (error.response) {
-            // Error if city name was not found or invalid input.
-            console.log(error.response.data);
-          } else if (error.request) {
-            // Error if no response was received.
-            console.log(error.request);
-          } else {
-            // Other errors.
-            console.log(error.message);
-          }
-        });
-    }
-  };
 
   const changeBackground = () => {
     let background = "App ";
@@ -59,17 +19,7 @@ const App = () => {
   return (
     <div className={changeBackground()}>
       <main>
-        <div className="search-box">
-          <input
-            type="text"
-            ref={inputRef}
-            className="search-bar"
-            placeholder="Search City"
-            onChange={(event) => setQuery(event.target.value)}
-            value={query}
-            onKeyPress={search}
-          />
-        </div>
+        <SearchBar searchProps={setWeather} />
 
         {typeof weather.main != "undefined" ? (
           <WeatherInfo weatherData={weather} />
---
+++ src/components/search-bar/SearchBar.js
@@ -0,0 +1,61 @@
+import React, { useEffect, useRef, useState } from "react";
+import axios from "axios";
+import "./search-bar.css";
+
+const SearchBar = ({ searchProps }) => {
+  const openWeatherMapAPI = {
+    key: "8713314e929a6ae57041a78e979b402d",
+    base: "http://api.openweathermap.org/data/2.5/",
+  };
+
+  const [query, setQuery] = useState("");
+  const inputRef = useRef();
+
+  useEffect(() => {
+    inputRef.current.focus();
+  });
+
+  const search = (evt) => {
+    if (evt.key === "Enter") {
+      axios
+        .get(
+          `${openWeatherMapAPI.base}weather?q=${query}&units=metric&APPID=${openWeatherMapAPI.key}`
+        )
+        .then((response) => {
+          setQuery("");
+          searchProps(response.data);
+          console.log(response.data);
+        })
+        .catch((error) => {
+          setQuery("");
+
+          if (error.response) {
+            // Error if city name was not found or invalid input.
+            console.log(error.response.data);
+          } else if (error.request) {
+            // Error if no response was received.
+            console.log(error.request);
+          } else {
+            // Other errors.
+            console.log(error.message);
+          }
+        });
+    }
+  };
+
+  return (
+    <div className="search-box">
+      <input
+        type="text"
+        ref={inputRef}
+        className="search-bar"
+        placeholder="Search City"
+        onChange={(event) => setQuery(event.target.value)}
+        value={query}
+        onKeyPress={search}
+      />
+    </div>
+  );
+};
+
+export default SearchBar;
