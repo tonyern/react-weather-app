4462ec66b284a63ed7e202c5c5d0466ee80b4861
Tony <tony.d.nguyen@protonmail.com>
2021-04-04 17:34:19 -0500

    Converted test files to TSX.

--- src/components/weather-display/__test__/WeatherInfo.test.tsx
+++ src/components/weather-display/__test__/WeatherInfo.test.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import { renderer } from "react-test-renderer";
 import "@testing-library/jest-dom/extend-expect";
 import WeatherInfo from "../WeatherInfo";
 
